826458495523dd4d34aec520cce9ab818df4d5ef
Ahmedzagzoug1 <zagzougprograming@gmail.com>
2026-06-15 09:14:40 +0300

    UPDATE auth controller and model

--- modules/auth/controllers/auth.controller.js
+++ modules/auth/controllers/auth.controller.js
@@ -1,14 +1,13 @@
-const User = require('../../../models/user_model');
-
-const asyncWrapper = require('../../../../shared/middleware/async_wrapper');
+const User = require('../models/user.model');
+const asyncWrapper = require('../../../shared/middleware/async_wrapper');
 const AppError = require('../../../shared/utils/app_error');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const HttpStatusText = require('../../../../shared/utils/http_status_text');
+const HttpStatusText = require('../../../shared/utils/http_status_text');
 const login = asyncWrapper(async (req, res, next) => {
-    const { email, password,role } = req.body;
-    const user = await User.findOne({ email });
-    console.log(user);
+    const { identifier, password } = req.body;
+    const user = await User.findOne({ $or: [{ email: identifier }, { mobile: identifier }],
+        password: { $exists: true } }).select('+password');
     if (!user) {
         return next(new AppError('Invalid email or password', 401));
     }
@@ -16,18 +15,27 @@ const login = asyncWrapper(async (req, res, next) => {
     if (!isMatch) {
         return next(new AppError('Invalid email or password', 401));
     }
-    const token = jwt.sign({ email: user.email,role: user.role },
-         process.env.JWT_SECRET, { expiresIn: '1h' });
-    res.status(200).json({status:HttpStatusText.Success,message: token });
+    const token =createAccessToken(user.email, user.role);
+    user.password = undefined; // Remove password from the response
+    res.status(200).json({status:HttpStatusText.Success,message: token ,data:{token,user} });
 });
 
 const register = asyncWrapper(async (req, res, next) => {
-    const { name, email, password, role } = req.body;
+    const { name, email, mobile, password, birthdate, role } = req.body;
+
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const user = new User({ name, email, password: hashedPassword, role,
+    const user = new User({ name, email, mobile,password: hashedPassword, role, birthdate,
          avatar:req.file ? req.file.filename : null });
     await user.save();
-    res.status(201).json({status:HttpStatusText.Success,message: 'User registered successfully' });
+
+    const token = createAccessToken(user.email, user.role);
+user.token = token;
+    res.status(201).json({status:HttpStatusText.Success,message: 'User registered successfully' ,
+    data:{token,user}
+    });
 });
+createAccessToken = (email, role) => {
+    return jwt.sign({ email, role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+};
 module.exports={login,register}; 
--- modules/auth/models/user.model.js
+++ modules/auth/models/user.model.js
@@ -1,10 +1,6 @@
 const {mongoose}=require('mongoose');
 const validator = require('validator');
-<<<<<<< HEAD
 const userRole = require('../../../shared/utils/user_role');
-=======
-const userRole = require('../shared/utils/user_role');
->>>>>>> master
 const userSchema=new mongoose.Schema({
     name:{type:String,required:true},
     email:{type:String,required:true,
@@ -13,19 +9,17 @@ const userSchema=new mongoose.Schema({
         lowercase:true,
         validate : [validator.isEmail, 'Please provide a valid email']
     },
-<<<<<<< HEAD
+    mobile:{type:String,required:true,
+        unique:true,
+        trim:true,
+        validate : [validator.isMobilePhone, 'Please provide a valid mobile number']
+    },
     birthdate:{type:Date,required:true},
     password:{type:String,required:true,
         trim:true,
+        select:false,
         validate : [validator.isLength, 'Password must be at least 6 characters long'
         ]            
-=======
-    password:{type:String,required:true,
-        trim:true,
-        validate : [validator.isLength, 'Password must be at least 6 characters long'
-            
-        ]
->>>>>>> master
     },
     avatar:{type:String,
         default:'/uploads/avatar.png'},
