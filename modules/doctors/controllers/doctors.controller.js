37a21749469e4f0376948995fb02e84bd989aabb
Ahmedzagzoug1 <zagzougprograming@gmail.com>
2026-06-23 16:16:41 +0300

    Complete doctor controller methods and update doctor 's module

--- modules/doctors/controllers/doctors.controller.js
+++ modules/doctors/controllers/doctors.controller.js
@@ -8,19 +8,12 @@ const {matchedData}=require('express-validator');
 
 
 const getAllDoctors=asyncWrapper(async(req,res,next)=>{
-<<<<<<< HEAD
 const doctors=await Doctor.find({});
 console.log(doctors.map((doctor)=>{
 doctor.gender;
 }));
-=======
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 const doctors=await Doctor.find({});
-=======
-=======
->>>>>>> Stashed changes
->>>>>>> doctors
+
 
   const sorted={};
 const {gender,rated}  =req.query;
@@ -28,12 +21,10 @@ const filter={};
 if(gender !=null){
     filter.gender=gender;
 
-<<<<<<< Updated upstream
 console.log(doctors.map((doctor)=>{
 doctor.gender;
 }));
     res.status(200).json({'status':HttpStatusText.Success,'message':'','data':doctors});
-=======
 }
 if(rated =='1'){
 sorted=1;
@@ -43,7 +34,6 @@ sorted=1;
     res.status(200).json({'status':HttpStatusText.Success,'message':'doctors get successfully',
         'results':doctors.length,
         'data':{doctors}});
->>>>>>> Stashed changes
 });
 const setAvailability=asyncWrapper(async(req,res,next)=>{
 
@@ -85,21 +75,12 @@ const doctor=Doctor.findByIdAndUpdate(id,updates,
 });
 
 const addDoctor=asyncWrapper(async(req,res,next)=>{
-<<<<<<< Updated upstream
-const{id,title,specialty,yearsOfExperience,focus,gander,profileDescription,careerPath,highlights}=req.body;
- 
-const user=await User.findById(id);
-console.log(user._id);
-const userObject=user.$toObject;
-const doctor=new Doctor.create( user._id,title,specialty,yearsOfExperience,focus,gander,profileDescription,careerPath,highlights);
-await doctor.save();   
-=======
+
 const{title,specialty,yearsOfExperience,focus,gender,profileDescription,careerPath,highlights}=req.body;
 const userDecoded=req.user;
 const doctor=await Doctor.create( {userId :userDecoded.id,title:title,
 specialty:  specialty,yearsOfExperience: yearsOfExperience,
 focus:focus,gender:gender,profileDescription:profileDescription,careerPath:careerPath,highlights:highlights});
->>>>>>> Stashed changes
 console.log(doctor._id);
 res.status(201).json({'status':HttpStatusText.Success,'message':'doctor \'s profile add successful','data':{doctor}});
 });
