d3af8dc4fddf8c98094c630c1d53808294d7abeb
Ahmedzagzoug1 <zagzougprograming@gmail.com>
2026-08-05 22:16:26 +0300

    ADD Firebase configuration

--- config/app.config.js
+++ config/app.config.js
@@ -5,5 +5,13 @@ module.exports = {
     dbUri: process.env.DB_CONNECTION,
     ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
     REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
-NODE_ENV:process.env.NODE_ENV
+NODE_ENV:process.env.NODE_ENV,
+//
+ API_KEY : process.env.API_KEY,
+  CARD: process.env.CARD_INTEGRATION_ID,
+  WALLET: process.env.WALLET_INTEGRATION_ID,
+  KIOSK: process.env.KIOSK_INTEGRATION_ID,
+
+ IFRAME_ID : process.env.IFRAME_ID,
+ PAYMOB_HMAC_SECRET : process.env.PAYMOB_HMAC_SECRET
 };
