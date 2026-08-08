9d6083fbb5d5cdf44f204a454418a13ed0eecf5c
Ahmedzagzoug1 <zagzougprograming@gmail.com>
2026-08-09 00:38:14 +0300

    UPATE patient model

--- modules/patient/models/patient.model.js
+++ modules/patient/models/patient.model.js
@@ -1,18 +1,18 @@
-const mongoose=require('mongoose');
-const patientSchema=new mongoose.Schema({
-userId: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'User', 
-        required: true,
-    unique: true
-  },
-  favoriteDoctors:[{
-    type: mongoose.Schema.ObjectId,
-    ref: 'Doctor', 
-    unique: true
-   
-  }]
-}
-);
-const Patient=mongoose.model('Patient',patientSchema);
-module.exports=Patient;
+      const mongoose=require('mongoose');
+      const patientSchema=new mongoose.Schema({
+      userId: {
+          type: mongoose.Schema.ObjectId,
+          ref: 'User', 
+              required: true,
+          unique: true
+        },
+        favoriteDoctors:[{
+          type: mongoose.Schema.ObjectId,
+          ref: 'Doctor', 
+          unique: true
+        
+        }]
+      }
+      );
+      const Patient=mongoose.model('Patient',patientSchema);
+      module.exports=Patient;
